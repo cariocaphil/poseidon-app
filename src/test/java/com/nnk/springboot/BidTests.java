5d2f8dc515f2cdc60edad69563366498d06dbd91
cariocaphil <cariocaphil@gmail.com>
2024-04-07 20:35:40 +0200

    add further tests

--- src/test/java/com/nnk/springboot/BidTests.java
+++ src/test/java/com/nnk/springboot/BidTests.java
@@ -2,16 +2,23 @@ package com.nnk.springboot;
 
 import com.nnk.springboot.domain.Bid;
 import com.nnk.springboot.repositories.BidListRepository;
+import com.nnk.springboot.services.BidListService;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 @SpringBootTest
 public class BidTests {
 
   @Autowired
   private BidListRepository bidListRepository;
+
+
+  @Autowired
+  private BidListService bidListService;
 
   @Test
   public void bidListTest() {
@@ -36,5 +43,19 @@ public class BidTests {
     // Verify
     Optional<Bid> deletedBid = bidListRepository.findById(bid.getId());
     Assertions.assertFalse(deletedBid.isPresent());*/
+  }
+
+  @Test
+  public void saveNullBidTest() {
+    Bid bid = null;
+    Assertions.assertThrows(InvalidDataAccessApiUsageException.class, () -> bidListService.save(bid));
+  }
+
+
+  @Test
+  public void findByIdInvalidIdTest() {
+    Integer invalidId = -1;
+    Optional<Bid> foundBid = bidListService.findById(invalidId);
+    Assertions.assertFalse(foundBid.isPresent());
   }
 }

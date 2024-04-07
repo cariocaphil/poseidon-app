d0778440321b5e1d89c43386eabe190eb8929e60
cariocaphil <cariocaphil@gmail.com>
2024-04-07 13:17:53 +0200

    update bid model class

--- src/main/java/com/nnk/springboot/domain/Bid.java
+++ src/main/java/com/nnk/springboot/domain/Bid.java
@@ -1,18 +1,65 @@
 package com.nnk.springboot.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
+import jakarta.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "bidlist")
 public class Bid {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
-
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private Integer bidListId;
+
+  private String account;
+
+  private String type;
+
+  @Column(name = "bid_quantity")
+  private Double bidQuantity;
+
+  @Column(name = "ask_quantity")
+  private Double askQuantity;
+
+  private Double bid;
+
+  private Double ask;
+
+  private String benchmark;
+
+  @Column(name = "bid_list_date")
+  private Timestamp bidListDate;
+
+  private String commentary;
+
+  private String security;
+
+  private String status;
+
+  private String trader;
+
+  private String book;
+
+  @Column(name = "creation_name")
+  private String creationName;
+
+  @Column(name = "creation_date")
+  private Timestamp creationDate;
+
+  @Column(name = "revision_name")
+  private String revisionName;
+
+  @Column(name = "revision_date")
+  private Timestamp revisionDate;
+
+  @Column(name = "deal_name")
+  private String dealName;
+
+  @Column(name = "deal_type")
+  private String dealType;
+
+  @Column(name = "source_list_id")
+  private String sourceListId;
+
+  private String side;
+
 }

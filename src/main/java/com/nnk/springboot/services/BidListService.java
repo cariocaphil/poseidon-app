5397660b1b4b8f470ff1bb543339863128480423
cariocaphil <cariocaphil@gmail.com>
2024-04-07 17:29:14 +0200

    add Transactional annotation

--- src/main/java/com/nnk/springboot/services/BidListService.java
+++ src/main/java/com/nnk/springboot/services/BidListService.java
@@ -2,19 +2,22 @@ package com.nnk.springboot.services;
 
 import com.nnk.springboot.domain.Bid;
 import com.nnk.springboot.repositories.BidListRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import org.tinylog.Logger;
 
-@Transactional
 @Service
 public class BidListService {
 
   private final BidListRepository bidListRepository;
+
+  @PersistenceContext
+  private EntityManager entityManager;
 
   @Autowired
   public BidListService(BidListRepository bidListRepository) {
@@ -28,10 +31,13 @@ public class BidListService {
   public Optional<Bid> findById(Integer id) {
     return bidListRepository.findById(id);
   }
-
+  @Transactional
   public Bid save(Bid bidList) {
     Logger.info("Saving bid to the database: {}", bidList);
-    return bidListRepository.save(bidList);
+    Bid savedBid = bidListRepository.save(bidList);
+    entityManager.flush();
+    entityManager.clear();
+    return savedBid;
   }
 
   public Bid update(Bid bidList) {

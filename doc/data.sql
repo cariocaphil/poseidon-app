ed8f176889eeebb68dd82bc3556dc8ce234331f6
cariocaphil <cariocaphil@gmail.com>
2024-06-07 19:50:59 +0200

    adjust data sql script

--- doc/data.sql
+++ doc/data.sql
@@ -87,9 +87,9 @@ CREATE TABLE RuleName (
 
 CREATE TABLE Users (
   Id tinyint(4) NOT NULL AUTO_INCREMENT,
-  username VARCHAR(125),
-  password VARCHAR(125),
-  fullname VARCHAR(125),
+  username VARCHAR(125) NOT NULL,
+  password VARCHAR(125) NOT NULL,
+  fullname VARCHAR(125) NOT NULL,
   role VARCHAR(125),
 
   PRIMARY KEY (Id)
